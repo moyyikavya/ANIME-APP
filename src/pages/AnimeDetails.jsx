e8101d2e36bc8452394eea278a65ed06bb20ce3a
moyyikavya <moyyikavya593@gmail.com>
2026-06-17 10:42:13 +0530

    latest commit

--- src/pages/AnimeDetails.jsx
+++ src/pages/AnimeDetails.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-import api from "../Services/api";
+import api from "../services/api";
 
 import ReviewForm from "../components/ReviewForm";
 
--- src/pages/EditAnime.jsx
+++ src/pages/EditAnime.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import api from "../Services/api";
+import api from "../services/api";
 
 function EditAnime() {
   const { id } = useParams();
